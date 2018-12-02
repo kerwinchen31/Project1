0c1e1afe9b030f52ca6563d6edd493913f4189f1
kerwinchen31 <kchen37@stuy.edu>
2018-12-02 03:15:32 -0500

    added files

--- main.c
+++ main.c
@@ -17,6 +17,36 @@ int run(char **args){
   }
   a = fork();
   if (!a){
+    /* for (int g = 0; g < 3; g++){ */
+    /*   printf("%s\n", args[g]); */
+    /* } */
+    /* if (!(strcmp(args[1], ">") && args[1])){ */
+    /*   int greater = 1; */
+    /*   int k = 0;/\* */
+    /* 		  for (; k < 100; k++){ */
+    /* 		  printf("hello\n"); */
+    /* 		  printf("yes %s\n", args[k]); */
+    /* 		  if (!strcmp(args[k],">")){ */
+    /* 		  greater = 1; */
+    /* 		  printf("GREATERR %d\n",greater);  */
+    /* 		  } */
+    /* 		  }*\/ */
+    /*   /\*char ** a[50]; */
+    /* 	for (int x = 0; x < k; x ++){ */
+    /* 	a[x] = args[x]; */
+    /* 	}*\/ */
+    /*   int fd; */
+    /*   int backup = dup(1); */
+    /*   fd = open(args[2], O_CREAT | O_WRONLY, 777); */
+    /*   dup2(fd, 1); */
+    /*   char * temp = args[0]; */
+    /*   printf("hello\n"); */
+    /*   close(fd); */
+    /*   dup2(backup, 1); */
+    /*   return 7; */
+    /* }else{ */
+    /*   return execvp(args[0], args); */
+    /* } */
     return execvp(args[0], args);
   }else{
     int status;
@@ -24,6 +54,7 @@ int run(char **args){
     return WEXITSTATUS(status);
   }
 }
+      
 
 int main(){
   int keepRunning = 1;
@@ -42,9 +73,11 @@ int main(){
 	//printf("GOTCHA\n");
 	exit(0);
       }
-      run(space_args(args[i]));
+      char ** input = space_args(args[i]);
+      run(input);
       // printf("%s", line);
     }
   }
+  
   return 0;
 }
